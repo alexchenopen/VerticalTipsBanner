95e2436f22176e208f2617619f7e7359b40bffd7
ChenAlex <alex.linchen@gmail.com>
2017-11-23 15:45:19 +0800

    0.0.5

--- banner/src/main/java/com/alex/widget/banner/tips/BannerConfig.java
+++ banner/src/main/java/com/alex/widget/banner/tips/BannerConfig.java
@@ -12,5 +12,5 @@ public class BannerConfig {
     public static final int TIME = 2000;
     public static final int DURATION = 800;
     public static final boolean IS_AUTO_PLAY = true;
-    public static final boolean IS_SCROLL = true;
+    public static final boolean IS_SCROLL = false;
 }
